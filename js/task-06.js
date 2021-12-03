d4c5def15d733c72b5869c57e03bb7ce53cb28b8
aleksandr105 <aleksandrsherbina105@gmail.com>
2021-12-03 21:08:39 +0100

    adds solution 6 and 7 tasks

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector("#validation-input");
+
+const onValidationInput = (event) => {
+  if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+};
+
+inputEl.addEventListener("blur", onValidationInput);
