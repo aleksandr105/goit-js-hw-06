8dde486e8fc7086e6e1dc57015075c5576cda82c
aleksandr105 <aleksandrsherbina105@gmail.com>
2021-12-04 20:09:47 +0100

    Add solution 8 and 9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const mailValueLength = event.currentTarget.email.value.length;
+  const passwordValueLength = event.currentTarget.password.value.length;
+
+  if (mailValueLength === 0 || passwordValueLength === 0) {
+    alert("Все поля должны быть заполнены!!!");
+  }
+
+  const objektForm = {
+    email: event.currentTarget.email.value,
+    password: event.currentTarget.password.value,
+  };
+  console.log(objektForm);
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const buttonRef = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+
+buttonRef.addEventListener("click", onColorBody);
+
+function onColorBody() {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  spanEl.textContent = `${getRandomHexColor()}`;
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
 }
